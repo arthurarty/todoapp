bd28b12a717120e97afef9c678fb6dbddd39698f
arthurarty <arthur.nangai@gmail.com>
2020-01-08 12:37:41 +0300

    ft(update) - Mark as complete.

--- app.py
+++ app.py
@@ -49,5 +49,20 @@ def create_todo():
         return jsonify(body)
 
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
