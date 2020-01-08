9c62737c6232651ab4c688a5e045ec8d6d1da8cb
arthurarty <arthur.nangai@gmail.com>
2020-01-08 16:19:03 +0300

    ft(todo) - Implement delete method.

--- app.py
+++ app.py
@@ -64,5 +64,18 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
